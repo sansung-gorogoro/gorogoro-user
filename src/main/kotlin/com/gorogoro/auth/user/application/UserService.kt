ceed51a65c29098dd5a77b7489f30b6c997df25d
dkstpwns10 <dkstpwns10@naver.com>
2025-12-04 16:03:18 +0900

    feature/user controller, service생성

---
+++ src/main/kotlin/com/gorogoro/auth/user/application/UserService.kt
@@ -0,0 +1,4 @@
+package com.gorogoro.auth.user.application
+
+interface UserService {
+}
---
+++ src/main/kotlin/com/gorogoro/auth/user/application/UserServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.gorogoro.auth.user.application
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserServiceImpl(): UserService {
+
+}
