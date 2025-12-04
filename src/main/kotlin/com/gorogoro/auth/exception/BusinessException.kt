c5a9a7e3a9dd51b474cfd664947d1b439d67757b
dkstpwns10 <dkstpwns10@naver.com>
2025-12-04 16:26:53 +0900

    feature/Exception추가 및 JwtProvider생성

---
+++ src/main/kotlin/com/gorogoro/auth/exception/BusinessException.kt
@@ -0,0 +1,4 @@
+package com.gorogoro.auth.exception
+
+class BusinessException {
+}
---
+++ src/main/kotlin/com/gorogoro/auth/jwt/JwtProvider.kt
@@ -0,0 +1,57 @@
+package com.gorogoro.auth.jwt
+
+import com.gorogoro.auth.user.domain.Role
+import io.jsonwebtoken.Jwts
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import javax.crypto.SecretKey
+import io.jsonwebtoken.security.Keys
+import java.time.Instant
+import java.util.Date
+
+@Component
+class JwtProvider(
+    @Value("\${jwt.secret}") private val secretKey: String,
+    @Value("\${jwt.expiration-ms}") private val accessExpirationHours: Long,
+    @Value("\${jwt.refresh-expiration-ms}") private val refreshExpirationDays: Long
+) {
+    private val key: SecretKey by lazy {
+        Keys.hmacShaKeyFor(secretKey.toByteArray(StandardCharsets.UTF_8))
+    }
+
+    fun createAccessToken(userId: Long, role: Role): String {
+        val now = Instant.now()
+        val validity = now.plusSeconds(accessExpirationHours * 3600)
+
+        return Jwts.builder()
+            .subject(userId.toString())
+            .claim("role", role.name)
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(validity))
+            .signWith(key)
+            .compact()
+    }
+
+    fun createRefreshToken(): Pair<String, Instant> {
+        val now = Instant.now()
+        val validity = now.plusSeconds(refreshExpirationDays * 24 * 3600) // 매우 긴 시간
+
+        val token = Jwts.builder()
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(validity))
+            .signWith(key)
+            .compact()
+
+        return Pair(token, validity)
+    }
+
+    fun validateToken(token: String): Boolean {
+        return try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
