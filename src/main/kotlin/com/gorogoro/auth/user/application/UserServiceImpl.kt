e774aad320cfd5b6b08da05b14e8095a4641ed73
dkstpwns10 <dkstpwns10@naver.com>
2025-12-07 23:44:07 +0900

    [feature/jwt] jwt검증 로직 및 파싱기능 분리

--- src/main/kotlin/com/gorogoro/auth/user/application/UserServiceImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.gorogoro.auth.user.application
-
-import org.springframework.stereotype.Service
-
-@Service
-class UserServiceImpl(): UserService {
-
-}
--- src/main/kotlin/com/gorogoro/auth/user/presentation/UserController.kt
+++
@@ -1,9 +0,0 @@
-package com.gorogoro.auth.user.presentation
-
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@RequestMapping("/user")
-class UserController {
-}
