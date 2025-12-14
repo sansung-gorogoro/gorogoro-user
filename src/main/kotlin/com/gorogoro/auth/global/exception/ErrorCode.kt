59b856d2c6a5a80b80c1672c259909f449e8fc60
dkstpwns10 <dkstpwns10@naver.com>
2025-12-14 13:11:57 +0900

    feat/ Added name validation code

--- src/main/kotlin/com/gorogoro/auth/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/gorogoro/auth/global/exception/ErrorCode.kt
@@ -29,6 +29,7 @@ enum class ErrorCode(
     NICKNAME_SAME_PREV(HttpStatus.BAD_REQUEST,"닉네임은 이전과 같습니다. 다시 입력해 주세요","US-0012"),
     USER_STATUS_IS_NOT_VALID(HttpStatus.BAD_REQUEST,"해당 유저는 휴면 또는 삭제 되었습니다.","US-0013"),
     USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST,"이미 가입된 유저 입니다.","US-0014"),
+    COMPETE_KOREAN_NAME(HttpStatus.BAD_REQUEST,"이름은 완성된 한글 문자만 입력할 수 있습니다.","US-0015"),
 
     //예기치 않은 글로벌 에러
     GLOBAL_ERROR_UNEXPECTED(HttpStatus.INTERNAL_SERVER_ERROR,"예기치 않은 에러가 발생했습니다. 문의팀에 문의를 남겨주세요.","GB-0001"),
