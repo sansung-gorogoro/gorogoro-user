e7e6610bf526264bf00639ba04f8d0711e85bb48
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-12-20 22:07:09 +0900

    Initial project setup

---
+++ src/main/java/com/gorogoro/auth/GorogoroUserApplication.java
@@ -0,0 +1,13 @@
+package com.gorogoro.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorogoroUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GorogoroUserApplication.class, args);
+    }
+
+}
